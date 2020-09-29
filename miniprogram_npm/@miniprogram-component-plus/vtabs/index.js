cc09f99e3f363deea9c1474d1d7eab8e5bca644c
qisk <13017358@qq.com>
2020-09-29 16:46:35 +0800

    通过vtabs的修改，支持商品延时加载

--- miniprogram_npm/@miniprogram-component-plus/vtabs/index.js
+++ miniprogram_npm/@miniprogram-component-plus/vtabs/index.js
@@ -122,21 +122,12 @@ Component({
             this.scrollTabBar(_activeTab);
         }
     },
+
     relations: {
         '../vtabs-content/index': {
             type: 'child',
             linked: function linked(target) {
-                var _this = this;
-
-                target.calcHeight(function (rect) {
-                    _this.data._contentHeight[target.data.tabIndex] = rect.height;
-                    if (_this._calcHeightTimer) {
-                        clearTimeout(_this._calcHeightTimer);
-                    }
-                    _this._calcHeightTimer = setTimeout(function () {
-                        _this.calcHeight();
-                    }, 100);
-                });
+                this.calcChildHeight(target)
             },
             unlinked: function unlinked(target) {
                 delete this.data._contentHeight[target.data.tabIndex];
@@ -147,6 +138,24 @@ Component({
         attached: function attached() {}
     },
     methods: {
+        hello: function hello(a) {
+            return a + 1
+        },
+        
+        calcChildHeight: function calcChildHeight(target) {
+            var _this = this;
+
+            target.calcHeight(function (rect) {
+                _this.data._contentHeight[target.data.tabIndex] = rect.height;
+                if (_this._calcHeightTimer) {
+                    clearTimeout(_this._calcHeightTimer);
+                }
+                _this._calcHeightTimer = setTimeout(function () {
+                    _this.calcHeight();
+                }, 100);
+            });
+        },
+
         calcHeight: function calcHeight() {
             var length = this.data.vtabs.length;
             var _contentHeight = this.data._contentHeight;
--- pages/category/index.js
+++ pages/category/index.js
@@ -4,20 +4,20 @@ Page({
   data: {
     vtabs: [],
     activeTab: 0,
-    showLoginPanel:false,
-    goodsListMap:{},
+    showLoginPanel: false,
+    goodsListMap: {},
   },
 
   async onLoad() {
     let categoriesData = await wxp.request_with_login({
       url: 'http://localhost:3000/goods/categories',
     })
-    if (categoriesData){
+    if (categoriesData) {
       // 注意，拿到categories要用两个data才能拿到
       categoriesData = categoriesData.data.data;
     }
     console.log(categoriesData);
-    
+
     // const titles = ['热搜推荐', '手机数码', '家用电器',
     //   '生鲜果蔬', '酒水饮料', '生活美食', 
     //   '美妆护肤', '个护清洁', '女装内衣', 
@@ -26,7 +26,7 @@ Page({
     //   '家居建材', '计生情趣', '医药保健', 
     //   '时尚钟表', '珠宝饰品', '礼品鲜花', 
     //   '图书音像', '房产', '电脑办公']
-    
+
     /*
     // 这种方式，会造成vtabs跳转位置不对，详见 http://localhost:88/web/#/page/edit/70/1717
     const vtabs = categoriesData.map(item => {
@@ -36,46 +36,72 @@ Page({
 
     this.setData({vtabs})
     */
-   
-   let vtabs = []
-    for(let j=0;j<categoriesData.length;j++){
+
+    let vtabs = []
+    for (let j = 0; j < categoriesData.length; j++) {
       let item = categoriesData[j]
-      await this.getGoodsListByCategory(item.id)
-      vtabs.push({title: item.category_name, id: item.id})
-    }
-    this.setData({vtabs})
-    
-    this.setData({test:111})
+      // 仅加载前三个类别的商品
+      if (j<3) this.getGoodsListByCategory(item.id, j)
+      // await this.getGoodsListByCategory(item.id)
+      vtabs.push({ title: item.category_name, id: item.id })
+    } 
+    this.setData({ vtabs })
+
+    this.setData({ test: 111 })
     console.log("this.data.test:", this.data.test)
   },
 
-  onTabCLick(e) {
+  onTabClick(e) {
     const index = e.detail.index
     console.log('tabClick', index)
+    this.onCategoryChanged(index)
   },
 
   onChange(e) {
     const index = e.detail.index
     console.log('change', index)
+    this.onCategoryChanged(index)
   },
 
-  async getGoodsListByCategory(categoryId){
+  onCategoryChanged(index){
+    let cate = this.data.vtabs[index]
+    let categoryId = cate.id
+    if (!this.data.goodsListMap[categoryId]){
+      this.getGoodsListByCategory(categoryId,index)
+    }
+  },
+
+  // 重新计算高度
+  reClacChildHeight(index) {
+    // 获取vtabs组件，调用扩展函数calcChildHeight
+    const goodsContent = this.selectComponent(`#goods-content${index}`)
+    console.log(goodsContent);
+
+    const categoryVtabs = this.selectComponent('#category-vtabs')
+    categoryVtabs.calcChildHeight(goodsContent)
+    //console.log("categoryVtabs.hello:", categoryVtabs.hello(100))
+  },
+
+  async getGoodsListByCategory(categoryId, index) {
     let goodsData = await wxp.request_with_login({
       url: `http://localhost:3000/goods/goods?page_index=1&page_size=20&category_id=${categoryId}`,
     })
-    if (goodsData){
+    if (goodsData) {
       goodsData = goodsData.data.data.rows;
     }
-    
-    console.log("before setData categoryId=", categoryId , "," ,this.data.goodsListMap[categoryId]);
-    
+
+    console.log("before setData categoryId=", categoryId, ",", this.data.goodsListMap[categoryId]);
+
     this.setData({
-      [`goodsListMap[${categoryId}]`]:goodsData
+      [`goodsListMap[${categoryId}]`]: goodsData
     })
-    
-    console.log("after setData categoryId=", categoryId , "," ,this.data.goodsListMap[categoryId]);
+
+    console.log("after setData categoryId=", categoryId, ",", this.data.goodsListMap[categoryId]);
 
     // qisk：这一句可以去掉，setData会自动更新
     //this.data.goodsListMap[categoryId] = goodsData
+
+    // 重新计算右侧的高度
+    this.reClacChildHeight(index)
   }
 })
