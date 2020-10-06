e76d95233c4021935fc5d6f80cb8305d92b8cd3a
qisk <13017358@qq.com>
2020-10-06 14:08:58 +0800

    支持商品规格的动态显示，并能根据所选规格获取到商品Sku信息（价格，库存等）

--- pages/goods/index.js
+++ pages/goods/index.js
@@ -1,4 +1,7 @@
 // pages/goods/index.js
+const { default: wxp } = require("../../lib/wxp")
+const util = require("../../utils/util.js")
+
 Page({
 
   /**
@@ -10,14 +13,19 @@ Page({
     goodsImages: [], // goodsImages是一个数组，所以用[]定义
     showSkuPanel: false, // 商品规格选择面板
     goodsContentInfo:{},
+    goodsSkuData:{}, // 有两个属性：goodsSku为sku表的内容，goodsAttrKeys为规格数据
+    selectedGoodsSku:{},
+    selectedAttrValue:{},
+    selectedGoodsSkuObject:{},
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: async function (options) {
     // options.goodsId,就是前面navigateTo中url传递的参数
     this.data.goodsId = options.goodsId
+    console.log("goods page: this.data.goodsId=", this.data.goodsId)
     // 接收goodsData事件，将商品数据设置到页面上
     const eventChannel = this.getOpenerEventChannel()
     eventChannel.on('goodsData', (res)=> {
@@ -33,6 +41,17 @@ Page({
         goodsContentInfo
       })
     })
+
+    // 拉取sku规格数据
+    let goodsSkuDataRes = await wxp.request_with_login({
+      url: util.ipAddress + `/goods/goods/${this.data.goodsId}/sku`
+    })
+    if (goodsSkuDataRes) {
+      let goodsSkuData = goodsSkuDataRes.data.data
+      this.setData({
+        goodsSkuData
+      })
+    }
   },
 
 
@@ -43,6 +62,73 @@ Page({
 
   onCloseSkuPanel() {
     this.setData({showSkuPanel:false})
+  },
+
+  onTapSkuTag(e) {
+    // 获取用户选择的规格
+    let attrvalue = e.currentTarget.dataset.attrvalue
+    let attrkey = e.currentTarget.dataset.attrkey
+
+    console.log('attravlueid', attrvalue, attrkey)
+    let selectedAttrValue = this.data.selectedAttrValue
+    selectedAttrValue[attrkey] = attrvalue
+    this.setData({
+      selectedAttrValue
+    })
+    console.log("selectedAttrValue:", selectedAttrValue)
+    // 计算价格及库存
+    let totalIdValue = 0
+    // 获取该商品所有的规格数据，将所选规格value的id取出相加，为的是和sku表的goods_attr_path做对应，获取价格和库存
+    let goodsAttrKeys = this.data.goodsSkuData.goodsAttrKeys
+
+    // 从selectedAttrValue中取出每种规格对应的规格值，并取出相加。
+    for (let j=0; j<goodsAttrKeys.length; j++) {
+      let attrName = goodsAttrKeys[j].attr_name
+      if (selectedAttrValue[attrName]) {
+        console.log("selectedAttrValue[attrName]:", selectedAttrValue[attrName])
+        totalIdValue += selectedAttrValue[attrName].id
+      }
+    }
+    console.log("totalIdValue:", totalIdValue)
+
+    let goodsSku = this.data.goodsSkuData.goodsSku
+    let tempTotalIdValue = 0
+    for (let j=0; j<goodsSku.length; j++) {
+      let goodsAttrPath = goodsSku[j].goods_attr_path
+      // 判断goodsAttrPath长度和该商品的规格长度是否一致
+      if (goodsAttrPath.length != goodsAttrKeys.length) {
+        break
+      }
+
+      // 计算该商品sku表中goodsAttrPath的累加值
+      tempTotalIdValue = 0
+      goodsAttrPath.forEach(item=>tempTotalIdValue+=item)
+      console.log("tempTotalIdValue", tempTotalIdValue)
+
+      // 如果选择的商品规格和goodsAttrPath对应上，则设置选择的商品sku
+      if (tempTotalIdValue == totalIdValue) {
+        let selectedGoodsSku = goodsSku[j]
+        this.setData({
+          selectedGoodsSku
+        })
+        break
+      }
+    }
+  },
+
+  onConfirmGoodsSku(){
+     let goodsSkuData = this.data.goodsSkuData
+     let selectedGoodsSkuObject = this.data.selectedGoodsSkuObject
+     selectedGoodsSkuObject.sku = Object.assign({}, this.data.selectedGoodsSku)
+     selectedGoodsSkuObject.text = ''
+     for (let j=0; j<goodsSkuData.goodsAttrKeys.length;j++) {
+       let item = goodsSkuData.goodsAttrKeys[j]
+       selectedGoodsSkuObject.text += this.data.selectedAttrValue[item.attr_name].attr_value + ' '
+     }
+     this.setData({
+       selectedGoodsSkuObject,
+       showSkuPanel: false
+     })
   },
 
   /**
